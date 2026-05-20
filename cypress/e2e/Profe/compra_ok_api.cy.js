1ccb979004f2cf78a1303d731b6f8dd70cc0223a
Reyes, Luka <307712@moron.gob.ar>
2026-05-20 19:43:46 -0300

    Subida faltantes CP profe

--- cypress/e2e/Profe/compra_ok_api.cy.js
+++ cypress/e2e/Profe/compra_ok_api.cy.js
@@ -1,25 +1,82 @@
-describe('Casos de prueba API Compra Exitosa',()=>{
-    it('Comprar carrito exitosamente',()=>{
+describe('Casos de prueba API Compra Exitosa', () => {
+
+    let token
+    let user_id
+
+    before(() => {
+
         cy.request({
-            method:'POST',
-            url:'https://app.bookdbqa.online/api/login',
-            body:{
-                "username":"lukareyes",
-                "password":"Automata67"
+            method: 'POST',
+            url: 'https://app.bookdbqa.online/api/login',
+            body: {
+                username: 'lukareyes',
+                password: 'Automata67'
             }
-        }).then((response)=>{
+
+        }).then((response) => {
+
             expect(response.status).to.eq(200)
-            const token = response.body.token
-            let user_id = response.body.userDetails.userId
+
+            token = response.body.token
+            user_id = response.body.userDetails.userId
+
             cy.log(token)
             cy.log(user_id)
-            cy.request({
-                method:'POST',
-                url:'https://app.bookdbqa.online/api/ShoppingCart/AddToCart/'+user_id+'/2',
-                headers:{
-                    'Authorization': `Bearer ${token}`
-                }
-            })
+
         })
+
     })
+
+    it('Comprar carrito exitosamente', () => {
+
+        cy.request({
+            method: 'POST',
+            url: `https://app.bookdbqa.online/api/ShoppingCart/AddToCart/${user_id}/2`,
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+
+        }).then((response) => {
+
+            expect(response.status).to.eq(200)
+
+        })
+
+    })
+
+    it('CheckOut', () => {
+
+        cy.request({
+            method: 'POST',
+            url: `https://app.bookdbqa.online/api/CheckOut/${user_id}`,
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            body: {"orderDetails":[{"book":{"bookId":2,"title":"Harry Potter and the Chamber of Secrets","author":"JKR","category":"Mystery","price":236,"coverFileName":"9d8f4978-0ef8-42d0-873a-4eb583439237HP2.jpg"},"quantity":1}],"cartTotal":236}
+
+        }).then((response) => {
+
+            expect(response.status).to.eq(200)
+
+        })
+
+    })
+
+    it('VerOrder', () => {
+
+        cy.request({
+            method: 'GET',
+            url: `https://app.bookdbqa.online/api/Order/${user_id}`,
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+
+        }).then((response) => {
+
+            expect(response.status).to.eq(200)
+
+        })
+
+    })
+
 })
--- cypress/e2e/Profe/compra_token.cy.js
+++ cypress/e2e/Profe/compra_token.cy.js
@@ -1,33 +1,81 @@
-/* 
-Título: API | Error al comprar carrito sin token
+describe('Casos de prueba API Compra Exitosa', () => {
 
-Descripción: El objetivo de este test es verificar que el endpoint de compra del carrito valide correctamente la autenticación del usuario y devuelva un error cuando no se proporciona el token JWT Bearer.
+    let token
+    let user_id
 
-Precondiciones:
+    before(() => {
 
-    Tener un UserId de un usuario registrado
-*/
-
-describe('Casos de prueba API Compra Exitosa',()=>{
-it('Comprar carrito exitosamente',()=>{
-        /* cy.request({
-            method:'POST',
-            url:'https://app.bookdbqa.online/api/login',
-            body:{
-                "username":"lukareyes",
-                "password":"Automata67"
+        cy.request({
+            method: 'POST',
+            url: 'https://app.bookdbqa.online/api/login',
+            body: {
+                username: 'lukareyes',
+                password: 'Automata67'
             }
-        }).then((response)=>{
+
+        }).then((response) => {
+
             expect(response.status).to.eq(200)
-            const token = response.body.token
-            let user_id = response.body.userDetails.userId
+
+            token = response.body.token
+            user_id = response.body.userDetails.userId
+
             cy.log(token)
-            cy.log(user_id) */
+            cy.log(user_id)
+
+        })
+
+    })
+
+    it('Comprar carrito exitosamente', () => {
+
+        cy.request({
+            method: 'POST',
+            url: `https://app.bookdbqa.online/api/ShoppingCart/AddToCart/${user_id}/2`,
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+
+        }).then((response) => {
+
+            expect(response.status).to.eq(200)
+
+        })
+
+    })
+
+    it('CheckOut', () => {
+
             cy.request({
-                method:'POST',
-                url:'https://app.bookdbqa.online/api/ShoppingCart/AddToCart/'+'1058'+'/2',
-            })
-        /* })
-    }) */
-})
+            method: 'POST',
+            url: `https://app.bookdbqa.online/api/CheckOut/${user_id}`,
+
+            failOnStatusCode: false,
+
+            body: {
+                "orderDetails": [
+                    {
+                        "book": {
+                            "bookId": 2,
+                            "title": "Harry Potter and the Chamber of Secrets",
+                            "author": "JKR",
+                            "category": "Mystery",
+                            "price": 236,
+                            "coverFileName": "9d8f4978-0ef8-42d0-873a-4eb583439237HP2.jpg"
+                        },
+                        "quantity": 1
+                    }
+                ],
+                "cartTotal": 236
+            }
+
+        }).then((response) => {
+
+            expect(response.status).to.eq(401)
+
+        })
+
+
+    })
+
 })
